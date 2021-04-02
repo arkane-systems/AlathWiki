1e61dcfeaa0749b083c46e0779ae18f7798401a8
Alistair Young <avatar@arkane-systems.net>
2021-04-02 09:39:56 -0500

    Add project files.

---
+++ AlathWiki/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿#region header
+
+// AlathWiki - App.xaml.cs
+// 
+// Created by: Alistair J R Young (avatar) at 2021/04/02 1:51 AM.
+
+#endregion
+
+#region using
+
+using System.Windows;
+
+#endregion
+
+namespace ArkaneSystems.AlathWiki.Desktop
+{
+    /// <summary>
+    ///     Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    { }
+}
---
+++ AlathWiki/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿#region header
+
+// AlathWiki - MainWindow.xaml.cs
+// 
+// Created by: Alistair J R Young (avatar) at 2021/04/02 1:51 AM.
+
+#endregion
+
+#region using
+
+using System.Windows;
+
+#endregion
+
+namespace ArkaneSystems.AlathWiki.Desktop
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow ()
+        {
+            this.InitializeComponent ();
+        }
+    }
+}
